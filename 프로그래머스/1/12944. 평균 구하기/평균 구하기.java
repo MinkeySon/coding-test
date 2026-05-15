7c6b9257c0ccc223ee8bcc6905b60b97b262653f
HandMK <minkeygod@kangnam.ac.kr>
2026-05-15 13:56:29 +0900

    [level 1] Title: 평균 구하기, Time: 1.30 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        int sum = Arrays.stream(arr).sum();
+        
+        answer = (double) sum / (double) arr.length;
+        
+        return answer;
+    }
+}
